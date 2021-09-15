1f1d97b9e13a60b456f649d14569cef267730d66
Alexander Prins <57255355+alex651@users.noreply.github.com>
2021-09-15 12:40:15 +0000

    veranderd

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
+var width = 400, height = 300;
+
 class Ball {
-  constructor(x,y,w,h,vx,vy) {
+  constructor(x,y,w,h,vx,vy,color) {
     this.x = x;
     this.y = y;
     this.w = w;
     this.h = h;
     this.vx = vx;
     this.vy = vy;
+    this.color = color
  }
 
-  drawball(){
-  createcanvas (300,300);
-  Background (225);
-  ellipse (x,y,50,50);
+    drawball(){
+    fill(this.color);
+    ellipse(this.x,this.y,this.w,this.h);
+    this.x = this.x + this.vx;
+    this.y = this.y + this.vy;
 
-  this.x = this.x + this.vx;
-  this.y = this.y + this.vy;
+    if(this.x < (this.w / 2) || this.x >= width - (this.w / 2)){
+      this.vx = this.vx * -1;
+    }
 
-  if(this.x < 0 || this.x > 300){
-    this.vx = this.vx * -1;
+    if(this.y < (this.h / 2) || this.y >= height - (this.h / 2)){
+      this.vy = this.vy * -1;
+    }
   }
 
-  if(this.y < 0 || this.y > 300){
-    this.vy = this.vy * -1;
-  }
- }
-}
-
-var ball1, ball2, ball3;
+var Ball1, Ball2, Ball3;
 
 function setup() {
 
-  ball1 = new Ball(30,200,50,50,5,5, "red");
+  Ball1 = new Ball(30,200,50,50,5,5, "red");
 
-  ball2 = new Ball(120,210,50,50,3,3, "blue");
+  Ball2 = new Ball(120,210,50,50,3,3, "blue");
 
-  ball3 = new Ball(200,150,50,50,1,1, "green");
+  Ball3 = new Ball(200,150,50,50,1,1, "green");
 
 }
 
@@ -44,6 +44,5 @@ function draw() {
   Ball2.draw();
   Ball3.draw();
 
-
-
+  }
 }
