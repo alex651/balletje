bb17b35e31c50709ddfbc5dab3ecb84db1db8a48
Alexander Prins <57255355+alex651@users.noreply.github.com>
2021-09-15 07:30:55 +0000

    1 stuiterend balletje

--- script.js
+++ script.js
@@ -1,39 +1,27 @@
-var x, y, r, v;
-function setup() {
-	createCanvas(400, 400);
-  y = 50;
-  x = 30;
-  r = 25;
-  v = 5;
-xspeed = 5;
-yspeed = 1;
-}
+var x, y, vx, vy;
 
+function setup() {
+	createCanvas(300, 300);
+
+  x = 30;
+  y = 200;
+  vx = 5;
+  vy = 5;
+}
 
 function draw() {
-	background(169);
+	background(225);
+  
+  ellipse(x,y,50,50);
+  x = x + vx;
+  y = y + vy;
 
-  circle(x, 130, 20);
-
-  x = x + v;
- 
-if(x < 0 || x > 400){
-   v = v * -1;
-}
-
-if(x < 0){
-  v = 5;
-}
-
-ellipse(x, y, r*2, r*2);
-  x += xspeed;
-  y += yspeed;
-  if (x > width - r || x < r) {
-    xspeed = -xspeed;
-  }
-  if (y > height - r || y < r) {
-    yspeed = -yspeed;
+  if(x < 0 || x > 300){
+    vx = vx * -1;
   }
 
+  if(y < 0 || y > 300){
+    vy = vy * -1;
+  }
 }
 
