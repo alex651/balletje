ce6469474b9c6e6780461e0308d38f8f58330ce2
Alexander Prins <57255355+alex651@users.noreply.github.com>
2021-09-15 08:05:48 +0000

    class

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-class ball {
+class Ball {
   constructor(x,y,w,h,vx,vy) {
     this.x = x;
     this.y = y;
@@ -8,15 +8,27 @@ class ball {
     this.vy = vy;
  }
 
- drawball(){
+  drawball(){
+  createcanvas (300,300);
+  Background (225);
+  ellipse (x,y,50,50);
 
+  this.x = this.x + this.vx;
+  this.y = this.y + this.vy;
+
+  if(this.x < 0 || this.x > 300){
+    this.vx = this.vx * -1;
+  }
+
+  if(this.y < 0 || this.y > 300){
+    this.vy = this.vy * -1;
+  }
  }
 }
 
 var ball1, ball2, ball3;
 
 function setup() {
-  createcanvas(400, 300);
 
   ball1 = new Ball(30,200,50,50,5,5, "red");
 
@@ -27,11 +39,10 @@ function setup() {
 }
 
 function draw() {
-  Background(225);
 
-  ball1.draw();
-  ball2.draw();
-  ball3.draw();
+  Ball1.draw();
+  Ball2.draw();
+  Ball3.draw();
 
 
 
